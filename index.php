05346e27baf5734fd22757747b45a73dca1b8aee
Matt Essam <matt@snapshotmedia.co.uk>
2012-03-01 18:46:20 +0000

    Converted style of app into PHP rather than just HTML

--- index.php
+++ index.php
@@ -132,50 +132,7 @@
 	</section><!-- End section feature-->
 	
     <section class="main">
-		<div id="recentevents">
-			<div id="recenteventheader">
-				<h3> Whats on tonight </h3>
-			</div>
-                <div class="eventcontainer">
-                	<div id="eventimg">
-			  <img src="Images/EventImages/Mint.gif"  alt="Hideout2012" />
-                	</div>
-                	<div id="eventtxt">
-			  <h2> Mint Mondays</h2>
-			  <span class="catagory">Venue:</span> The mint club <br>
-			  <span class="catagory">Entry: </span> &pound 5 <br>
-			  <span class="catagory">Music:</span> Mixed Genres <br>
-			  <span class="catagory">Doors:</span> 10:30pm-3:00am <br>
-			  
-			  <a href="#">View Event</a>
-			  
-			  
-			  
-                	</div>
-                	<div class="line1">
-                	</div>
-                	<div class="line2">
-                	</div>
-				</div> <!--end event container--> 
-			</div><!-- End event header-->
-                <div id ="news">
-                  <div id="recenteventheader">
-                    <h3> News </h3>
-                  </div>
-                  <div id="newshead">
-                    <h4> Hideout Festival 2012 - Lineup</h4>
-                  </div>
-                  <div id="newsimg">
-		    <img src="Images/newsimg.png" height=150 width=100% alt="Hideout2012" />
-                  </div>
-                  <div id="newstxt">
-                   <p> Tickets for Hideout festival 2012 are now on sale.<br> <a href="#">Get yours here!</a> </p> 
-                  </div>
-                  
-                  
-                </div>
-                
-    <div class="clear"></div>
+		<?php echo include('includes/' . $_GET['p'] . '.php')?>
     </section><!--end main-->
     
     
